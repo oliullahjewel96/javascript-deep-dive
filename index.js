10fa2d67407d8e65c3e80628b64c14bcfdb987ed
oli ullah <oli.jewel@northsouth.edu>
2022-11-30 16:33:09 +0600

    first coding challenge

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-var username = "oli";
+// var username = "oli";
 
-console.log(username)
+// console.log(username)
 
 // 1) Names only contains, letters, numbers, symnbols $ _
 // 2) first character must not be number
 
 // var firstname-; //invalid way to declare a variable
 // var 1lastName; //invalid way to declare a variable
+
+// Fix the variable names below, and log them out to the console!
+
+var firstName = "oli";
+
+var surName = "ullah";
+
+var oneThousand = "one thousand";
+
+console.log(firstname);
+console.log(surname);
+console.log(oneThousand);
