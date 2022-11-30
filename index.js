dd33ed56c0a0085e2a38f2c61e1d12c1bb83c81d
oli ullah <oli.jewel@northsouth.edu>
2022-11-30 19:04:38 +0600

    Why const improve your codes

--- index.js
+++ index.js
@@ -38,15 +38,28 @@
 
 
 // Challenge 1: We want to log out the name "oli", but it doesn't work. Can you fix it?
-const name = "Oli";
-console.log(name);
+// const name = "Oli";
+// console.log(name);
 
-// Challenge 2: We want 102 to be logged out. But we're getting an error instead. Try to fix it!
-let count = 100;
-count = 101;
-count = 102;
-console.log(count);
+// // Challenge 2: We want 102 to be logged out. But we're getting an error instead. Try to fix it!
+// let count = 100;
+// count = 101;
+// count = 102;
+// console.log(count);
 
-//  Challenge 3: We want to log out the price (50), but it doesn't work. Can you fix it?
-let price = 50;
-console.log(price);
+// //  Challenge 3: We want to log out the price (50), but it doesn't work. Can you fix it?
+// let price = 50;
+// console.log(price);
+
+
+// const - restrictions that make code more readable
+// 1) must be initialized with value
+//2) can't be reassigned after declarations
+
+const originalPrice = 50;
+const percentOff = 20;
+const salePrice = originalPrice * (percentOff / 100);
+
+// rest of our program (maybe 100s of lines)
+
+console.log(salePrice);
