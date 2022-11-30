cd9c1a029631c5c87e282b7317e911695ca7bef1
oli ullah <oli.jewel@northsouth.edu>
2022-11-30 15:52:18 +0600

    Variables with basics

---
+++ index.js
@@ -0,0 +1,9 @@
+var username = "oli";
+
+console.log(username)
+
+// 1) Names only contains, letters, numbers, symnbols $ _
+// 2) first character must not be number
+
+// var firstname-; //invalid way to declare a variable
+// var 1lastName; //invalid way to declare a variable
