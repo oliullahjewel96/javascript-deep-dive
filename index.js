8b36d23e5a940f269ba7d3e52ff64aa06424f97c
oli ullah <oli.jewel@northsouth.edu>
2022-12-13 13:10:37 +0600

    Conditionals short circuiting

--- index.js
+++ index.js
@@ -161,8 +161,46 @@ everything else - Object type
 
 // ternary chaining
 
-const age = 12;
+// const age = 12;
 
-const greetings = age < 12 ? "Hey Kiddo" : age < 18 ? "Hey Teenager" : age > 20 ? "hey adult" : "Hey man"
+// const greetings = age < 12 ? "Hey Kiddo" : age < 18 ? "Hey Teenager" : age > 20 ? "hey adult" : "Hey man"
 
-console.log(greetings);
+// console.log(greetings);
+
+// conditionals short circuiting
+
+// const name = "oli"
+// const isEmailVerified = true;
+
+// const username = -isEmailVerified && name  || "Invalid user";
+
+// console.log(username);
+
+/* Challenge 1: Let's say you're building Reddit. Only users that are either moderators or have reached a certain karma threshold are allowed to upvote. */
+
+// const karma = 143;
+// const isModerator = true;
+
+// // first use a ternary to set hasEnoughKarma to true if karma is above 100. If not, set it to false
+// let hasEnoughKarma = karma > 100 ? true : false;
+
+// //  set the value of canUpvote with short-circuiting (using hasEnoughKarma and isModerator)
+// let canUpvote = isModerator || hasEnoughKarma;
+
+// console.log("canUpvote:", canUpvote);
+
+
+// /* Challenge 2: Let's say that only moderators who have enough karma are allowed to delete posts.  How would you set the canDelete variable */
+
+// // set canDelete to true if both hasEnoughKarma and isModerator is true
+// let canDelete = isModerator && hasEnoughKarma ;
+
+// console.log("canDelete:", canDelete);
+
+
+// // Challenge 3: what is the value of user? What if you switch the hasValidEmail variable to false? What if the response is empty?
+
+// const response = "";
+// const hasValidEmail = false;
+// const user = hasValidEmail && (response || "guest");
+//  console.log("user: ", user);
