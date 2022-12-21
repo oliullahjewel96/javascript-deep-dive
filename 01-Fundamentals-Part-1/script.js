0200ac264515082b8ae00cf4cf93a7537aa6deeb
oli ullah <oli.jewel@northsouth.edu>
2022-12-21 16:25:29 +0600

    Variable naming convention

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,13 @@
+let js = "amazing";
+
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "oli";
+let last_name = "Ullah";
+let birthYear96 = "june, 17, 1996";
+let income$ = "200$";
+
+console.log(firstName);
+console.log(last_name);
+console.log(birthYear96);
+console.log(income$);
