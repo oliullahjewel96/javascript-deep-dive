1cbb4cba9aaf61e47029641864ee0dfc8ee7ab6f
oli ullah <oli.jewel@northsouth.edu>
2022-12-21 17:00:13 +0600

    undefined and null data types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -8,8 +8,12 @@ let birthYear96 = "june, 17, 1996";
 let income$ = 200;
 let isCitizenOfBangladesh = true;
 
+let year;
+
 console.log(typeof firstName);
 console.log(typeof last_name);
 console.log(typeof birthYear96);
 console.log(typeof income$);
 console.log(typeof isCitizenOfBangladesh);
+console.log(typeof year);
+console.log(typeof null);
