820a34ea5cb88aaec6fe261518d250132f094981
oli ullah <oli.jewel@northsouth.edu>
2022-12-21 16:42:26 +0600

    typeof operator

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -5,9 +5,11 @@ console.log(40 + 8 + 23 - 10);
 let firstName = "oli";
 let last_name = "Ullah";
 let birthYear96 = "june, 17, 1996";
-let income$ = "200$";
+let income$ = 200;
+let isCitizenOfBangladesh = true;
 
-console.log(firstName);
-console.log(last_name);
-console.log(birthYear96);
-console.log(income$);
+console.log(typeof firstName);
+console.log(typeof last_name);
+console.log(typeof birthYear96);
+console.log(typeof income$);
+console.log(typeof isCitizenOfBangladesh);
