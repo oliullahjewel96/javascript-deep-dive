3da753b9fa2709f78d1b1cef12c18fa66f123827
oli ullah <oli.jewel@northsouth.edu>
2022-12-29 23:42:58 +0600

    Functions with and without parameters and arguments

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,1 +1,21 @@
 // Functions
+
+// function logger() {
+//   console.log("My name is Oli ullah Jewel");
+// }
+
+// //Calling / running / invoking a function
+// logger();
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
