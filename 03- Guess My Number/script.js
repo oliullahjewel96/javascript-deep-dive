e5a6e5b9d2d916962c73795c2bef82baac6e8025
oli ullah <oli.jewel@northsouth.edu>
2023-01-20 11:12:06 +0600

    DOM starter files

--- 03- Guess My Number/script.js
+++ 03- Guess My Number/script.js
@@ -1,1 +1,13 @@
-'use strict';
+"use strict";
+
+// const message = document.querySelector(".message");
+
+// message.textContent = "Correct Number!";
+
+// console.log(message, typeof message);
+
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 10;
+
+// document.querySelector(".guess").value = 23;
+// console.log(document.querySelector(".guess").value);
