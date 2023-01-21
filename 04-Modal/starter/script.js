4876499ad5ea071420e5d5a96ab40b5098ece4ad
oli ullah <oli.jewel@northsouth.edu>
2023-01-21 23:30:29 +0600

    Key down event using ESC key

--- 04-Modal/starter/script.js
+++ 04-Modal/starter/script.js
@@ -1,1 +1,30 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const showModal = document.querySelectorAll(".show-modal");
+const closeModal = document.querySelector(".close-modal");
+
+const openModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener("click", openModal);
+}
+
+const clickCloseModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+closeModal.addEventListener("click", clickCloseModal);
+overlay.addEventListener("click", clickCloseModal);
+
+document.addEventListener("keydown", (e) => {
+  console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    clickCloseModal();
+  }
+});
