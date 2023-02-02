9f071a92e31cd2862c85381c3edaba65e81cd38c
oli ullah <oli.jewel@northsouth.edu>
2023-02-02 23:39:40 +0600

    Refactored the game code

--- 05-Pig Game/script.js
+++ 05-Pig Game/script.js
@@ -6,37 +6,29 @@ let dice = document.querySelector(".dice");
 const btnRoll = document.querySelector(".btn--roll");
 const currentZero = document.querySelector("#current--0");
 const playerActive = document.querySelector(".player");
+const playerZero = document.querySelector(".player--0");
+const playerOne = document.querySelector(".player--1");
 
 scoreZero.textContent = 0;
 scoreOne.textContent = 0;
 dice.classList.add("hidden");
 let sum = 0;
-const rollDice = () => {
+let rollDice = () => {
+  //generate random number
   let randomNumber = Math.trunc(Math.random() * 6 + 1);
 
   console.log(randomNumber);
-  if (randomNumber === 1) {
-    dice.classList.remove("hidden");
-    dice.src = "dice-1.png";
-    playerActive.classList.remove("player--active");
-  } else if (randomNumber === 2) {
-    dice.classList.remove("hidden");
-    dice.src = "dice-2.png";
-  } else if (randomNumber === 3) {
-    dice.classList.remove("hidden");
-    dice.src = "dice-3.png";
-  } else if (randomNumber === 4) {
-    dice.classList.remove("hidden");
-    dice.src = "dice-4.png";
-  } else if (randomNumber === 5) {
-    dice.classList.remove("hidden");
-    dice.src = "dice-5.png";
+  //display dice element corresponding to the random number
+  dice.classList.remove("hidden");
+  dice.src = `dice-${randomNumber}.png`;
+  //check for rolled 1
+  if (randomNumber !== 1) {
+    //Add to current number
+    sum = sum + randomNumber;
+    currentZero.textContent = sum;
   } else {
-    dice.classList.remove("hidden");
-    dice.src = "dice-6.png";
+    //switch to next player
   }
-  sum = sum + randomNumber;
-  currentZero.textContent = sum;
 };
 
 // let randomDiceNumber = rollDice();
