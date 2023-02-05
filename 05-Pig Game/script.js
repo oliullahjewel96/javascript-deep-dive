66715d3dfe2f15840f89d1f77c0eb2e1b3a94b30
oli ullah <oli.jewel@northsouth.edu>
2023-02-05 08:46:56 +0600

    Solved switching player + cirrent score problem

--- 05-Pig Game/script.js
+++ 05-Pig Game/script.js
@@ -4,15 +4,16 @@ let scoreZero = document.querySelector("#score--0");
 let scoreOne = document.querySelector("#score--1");
 let dice = document.querySelector(".dice");
 const btnRoll = document.querySelector(".btn--roll");
-const currentZero = document.querySelector("#current--0");
-const playerActive = document.querySelector(".player");
-const playerZero = document.querySelector(".player--0");
-const playerOne = document.querySelector(".player--1");
+const currentZero = document.getElementById("current--0");
+const currentOne = document.getElementById("current--1");
 
 scoreZero.textContent = 0;
 scoreOne.textContent = 0;
 dice.classList.add("hidden");
+
+const scores = [0, 0];
 let sum = 0;
+let activePlayer = 0;
 let rollDice = () => {
   //generate random number
   let randomNumber = Math.trunc(Math.random() * 6 + 1);
@@ -25,9 +26,13 @@ let rollDice = () => {
   if (randomNumber !== 1) {
     //Add to current number
     sum = sum + randomNumber;
-    currentZero.textContent = sum;
+    document.getElementById(`current--${activePlayer}`).textContent = sum;
   } else {
     //switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    sum = 0;
+
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
 };
 
