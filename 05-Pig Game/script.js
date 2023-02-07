57753c0c6afa0689a5db1db1ea3cd4c5bf15394b
oli ullah <oli.jewel@northsouth.edu>
2023-02-08 01:37:28 +0600

    Finished coding the game

--- 05-Pig Game/script.js
+++ 05-Pig Game/script.js
@@ -10,15 +10,27 @@ const playerActive = document.querySelector(".player--active");
 const playerZero = document.querySelector(".player--0");
 const playerOne = document.querySelector(".player--1");
 const holdButton = document.querySelector(".btn--hold");
+const buttonNew = document.querySelector(".btn--new");
 
-scoreZero.textContent = 0;
-scoreOne.textContent = 0;
-dice.classList.add("hidden");
+let scores, sum, activePlayer, isPlaying;
+const init = () => {
+  scores = [0, 0];
+  sum = 0;
+  activePlayer = 0;
+  isPlaying = true;
 
-const scores = [0, 0];
-let sum = 0;
-let activePlayer = 0;
+  scoreZero.textContent = 0;
+  scoreOne.textContent = 0;
+  currentZero.textContent = 0;
+  currentOne.textContent = 0;
+  dice.classList.add("hidden");
+  playerZero.classList.remove("player--winner");
+  playerOne.classList.remove("player--winner");
+  playerZero.classList.add("player--active");
+  playerOne.classList.remove("player--active");
+};
 
+init();
 const switchPlayer = () => {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   sum = 0;
@@ -27,50 +39,54 @@ const switchPlayer = () => {
   playerOne.classList.toggle("player--active");
 };
 let rollDice = () => {
-  //generate random number
-  let randomNumber = Math.trunc(Math.random() * 6 + 1);
+  if (isPlaying) {
+    //generate random number
+    let randomNumber = Math.trunc(Math.random() * 6 + 1);
 
-  console.log(randomNumber);
-  //display dice element corresponding to the random number
-  dice.classList.remove("hidden");
-  dice.src = `dice-${randomNumber}.png`;
-  //check for rolled 1
-  if (randomNumber !== 1) {
-    //Add to current number
-    sum = sum + randomNumber;
-    document.getElementById(`current--${activePlayer}`).textContent = sum;
-  } else {
-    //switch to next player
-    switchPlayer();
+    console.log(randomNumber);
+    //display dice element corresponding to the random number
+    dice.classList.remove("hidden");
+    dice.src = `dice-${randomNumber}.png`;
+    //check for rolled 1
+    if (randomNumber !== 1) {
+      //Add to current number
+      sum = sum + randomNumber;
+      document.getElementById(`current--${activePlayer}`).textContent = sum;
+    } else {
+      //switch to next player
+      switchPlayer();
+    }
   }
-  // if (activePlayer === 1) {
-  //   playerZero.classList.remove("player--active");
-  //   playerOne.classList.add("player--active");
-  // } else {
-  //   playerOne.classList.remove("player--active");
-  //   playerZero.classList.add("player--active");
-  // }
 };
 
-// let randomDiceNumber = rollDice();
-
-// const addToCurrentScore = (numberToAdd) => {
-//   let sum = 0;
-//   sum = sum + numberToAdd;
-//   currentZero.textContent = sum;
-// };
-
 const holdDice = () => {
-  //Add current score to active player score's
-  scores[activePlayer] += sum;
-  document.getElementById(`score--${activePlayer}`).textContent =
-    scores[activePlayer];
-  console.log(scores);
-  //check if player's score is >=100
+  if (isPlaying) {
+    //Add current score to active player score's
+    scores[activePlayer] += sum;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    console.log(scores);
+    //check if player's score is >=100
+    if (scores[activePlayer] >= 10) {
+      isPlaying = false;
+      dice.classList.add("hidden");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      //switch to the next player
+      switchPlayer();
+    }
+  }
+};
 
-  //swith to the next player
-  switchPlayer();
+const newGame = () => {
+  init();
 };
 
 btnRoll.addEventListener("click", rollDice);
 holdButton.addEventListener("click", holdDice);
+buttonNew.addEventListener("click", newGame);
