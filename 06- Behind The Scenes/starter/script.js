2b076d3be737f24a503430d4308337d2855976d9
oli ullah <oli.jewel@northsouth.edu>
2023-02-08 15:00:04 +0600

    Scope chaining in js

--- 06- Behind The Scenes/starter/script.js
+++ 06- Behind The Scenes/starter/script.js
@@ -1,1 +1,22 @@
-'use strict';
+"use strict";
+
+function calcAge(birthYear) {
+  const age = 2023 - birthYear;
+
+  function printAge() {
+    const output = `${firstName} You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1991 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+    }
+    console.log(millenial);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = "oli";
+calcAge(1996);
