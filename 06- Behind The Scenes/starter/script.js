6c9f4dca5f1bdc966edbf4385695c9b53c041c5f
oli ullah <oli.jewel@northsouth.edu>
2023-02-09 08:14:15 +0600

    scenarios of this keyword

--- 06- Behind The Scenes/starter/script.js
+++ 06- Behind The Scenes/starter/script.js
@@ -1,22 +1,58 @@
 "use strict";
 
+// function calcAge(birthYear) {
+//   const age = 2023 - birthYear;
+
+//   function printAge() {
+//     const output = `${firstName} You are ${age}, born in ${birthYear}`;
+//     console.log(output);
+
+//     if (birthYear >= 1991 && birthYear <= 1996) {
+//       var millenial = true;
+//       const str = `oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
+//     }
+//     console.log(millenial);
+//   }
+//   printAge();
+//   return age;
+// }
+
+// const firstName = "oli";
+// calcAge(1996);
+
+//thid keyword
+
+console.log(this);
+
 function calcAge(birthYear) {
-  const age = 2023 - birthYear;
-
-  function printAge() {
-    const output = `${firstName} You are ${age}, born in ${birthYear}`;
-    console.log(output);
-
-    if (birthYear >= 1991 && birthYear <= 1996) {
-      var millenial = true;
-      const str = `oh, and you're a millenial, ${firstName}`;
-      console.log(str);
-    }
-    console.log(millenial);
-  }
-  printAge();
-  return age;
+  console.log(2023 - birthYear);
+  console.log(this);
 }
 
-const firstName = "oli";
 calcAge(1996);
+
+const calcAgeArrow = (birthYear) => {
+  console.log(2023 - birthYear);
+  console.log(this);
+};
+
+calcAgeArrow(1995);
+
+const oli = {
+  birthYear: 1996,
+  calcAge: function () {
+    console.log(this);
+    console.log(2023 - this.birthYear);
+  },
+};
+
+oli.calcAge();
+
+const muniya = {
+  birthYear: 2001,
+};
+
+muniya.calcAge = oli.calcAge;
+
+muniya.calcAge();
