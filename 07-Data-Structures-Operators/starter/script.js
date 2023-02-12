cd813421512069d8e27b4a7b7bb4fc508535fffb
oli ullah <oli.jewel@northsouth.edu>
2023-02-12 08:59:27 +0600

    default values in array destructuring

--- 07-Data-Structures-Operators/starter/script.js
+++ 07-Data-Structures-Operators/starter/script.js
@@ -50,3 +50,8 @@ const nested = [2, 4, [5, 6]];
 const [i, , [j, k]] = nested;
 
 console.log(i, j, k);
+
+//default values in array destructuring
+
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
