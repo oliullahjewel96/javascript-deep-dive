7e2073ee04d1d18d7a83bf528a091fa09998fdc3
oli ullah <oli.jewel@northsouth.edu>
2023-02-22 20:34:29 +0600

    split slice toUpperCase join methods of string

--- 07-Data-Structures-Operators/starter/script.js
+++ 07-Data-Structures-Operators/starter/script.js
@@ -521,3 +521,17 @@ const [...scored] = game.scored;
 // const [firstName, lastName] = 'Oli Ullah'.split(' ');
 // console.log(firstName);
 // console.log(lastName);
+
+// const capitalizeName = name => {
+//   const names = name.split(' ');
+//   const namesUpper = [];
+
+//   for (const n of names) {
+//     // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+//     namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+//   }
+//   console.log(namesUpper.join(' '));
+// };
+
+// capitalizeName('robin jobaer');
+// capitalizeName('oli ullah');
