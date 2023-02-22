ec4d4ed60fb3be2e0c942551573efde01c27d1f2
oli ullah <oli.jewel@northsouth.edu>
2023-02-22 22:22:36 +0600

    passing default value in function parameter

--- 08-Funtions/starter/script.js
+++ 08-Funtions/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = (flightNum, numPassengers = 1, price = 200) => {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
