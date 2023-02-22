55f00ab167bb4ed87517ec6167cf3c324215e790
oli ullah <oli.jewel@northsouth.edu>
2023-02-22 22:45:20 +0600

    Passing by value vs reference

--- 08-Funtions/starter/script.js
+++ 08-Funtions/starter/script.js
@@ -1,15 +1,38 @@
 'use strict';
 
-const bookings = [];
+// const bookings = [];
 
-const createBooking = (flightNum, numPassengers = 1, price = 200) => {
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
-};
+// const createBooking = (flightNum, numPassengers = 1, price = 200) => {
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
 
-createBooking('LH123');
+// createBooking('LH123');
+
+// const flight = 'LH123';
+
+// const oli = {
+//   fullName: 'Oli Ullah',
+//   passport: 'BF08234',
+// };
+
+// const checkIn = (flightNum, passenger) => {
+//   flightNum = 'LH234';
+
+//   passenger.fullName = 'Mr.' + passenger.fullName;
+
+//   if (passenger.passport === 'BF08234') {
+//     alert('Checked in');
+//   } else {
+//     console.log('Something wrong with your passport');
+//   }
+// };
+
+// checkIn(flight, oli);
+// console.log(flight);
+// console.log(oli);
