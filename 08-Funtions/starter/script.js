05babe10cc52a7b4dfd161b83779d6f2d7a078db
oli ullah <oli.jewel@northsouth.edu>
2023-03-02 23:21:22 +0600

    immediately invoked function

--- 08-Funtions/starter/script.js
+++ 08-Funtions/starter/script.js
@@ -143,42 +143,48 @@
 
 //Coding challenge
 
-const poll = {
-  question: 'What is your favorite programming language?',
-  option: ['0: Javascript', '1: Python', '2: Rust', '3: C++'],
-  answers: new Array(4).fill(0),
-  registerNewAnswer() {
-    // let arrayEl = '';
-    // for (const [i, el] of this.option.entries()) {
-    //   arrayEl += el + '\n';
-    // }
-    // console.log(arrayEl);
-    //Get input from user
-    const userInput = Number(
-      prompt(
-        `${this.question} \n ${this.option.join('\n')} \n(Write option number)`
-      )
-    );
+// const poll = {
+//   question: 'What is your favorite programming language?',
+//   option: ['0: Javascript', '1: Python', '2: Rust', '3: C++'],
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer() {
+//     // let arrayEl = '';
+//     // for (const [i, el] of this.option.entries()) {
+//     //   arrayEl += el + '\n';
+//     // }
+//     // console.log(arrayEl);
+//     //Get input from user
+//     const userInput = Number(
+//       prompt(
+//         `${this.question} \n ${this.option.join('\n')} \n(Write option number)`
+//       )
+//     );
 
-    typeof userInput === 'number' &&
-      userInput < this.answers.length &&
-      this.answers[userInput]++;
-    this.displayResult();
-    this.displayResult('string');
-  },
-  displayResult(type = 'array') {
-    if (type === 'array') {
-      console.log(this.answers);
-    } else if (type === 'string') {
-      console.log(`Poll results are ${this.answers.join(', ')}`);
-    }
-  },
-};
+//     typeof userInput === 'number' &&
+//       userInput < this.answers.length &&
+//       this.answers[userInput]++;
+//     this.displayResult();
+//     this.displayResult('string');
+//   },
+//   displayResult(type = 'array') {
+//     if (type === 'array') {
+//       console.log(this.answers);
+//     } else if (type === 'string') {
+//       console.log(`Poll results are ${this.answers.join(', ')}`);
+//     }
+//   },
+// };
 
-// poll.registerNewAnswer();
+// // poll.registerNewAnswer();
 
-document
-  .querySelector('.poll')
-  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+// document
+//   .querySelector('.poll')
+//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
-poll.displayResult.call({ answers: [5, 2, 3] }, 'string');
+// poll.displayResult.call({ answers: [5, 2, 3] }, 'string');
+
+// //IFFE
+
+(function () {
+  console.log('This will run only once');
+})();
