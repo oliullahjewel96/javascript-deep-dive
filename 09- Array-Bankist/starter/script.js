31872e892ff4c4c02cebc6dfb5e9b44a6a0bb179
oli ullah <oli.jewel@northsouth.edu>
2023-03-08 11:35:24 +0600

    fundamental difference between for of and forEach loop

--- 09- Array-Bankist/starter/script.js
+++ 09- Array-Bankist/starter/script.js
@@ -73,6 +73,24 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log("------FOREACH LOOP------");
+
+movements.forEach((mov, i, arr) => {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
 /////////////////////////////////////////////////
 
 // const arr = ["a", "b", "c", "d", "e"];
