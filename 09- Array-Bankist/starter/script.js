841f52b9e7edd28bf9485b8e1a254188f2847520
oli ullah <oli.jewel@northsouth.edu>
2023-03-18 23:05:01 +0600

    maximum number using reduce method

--- 09- Array-Bankist/starter/script.js
+++ 09- Array-Bankist/starter/script.js
@@ -242,3 +242,17 @@ const currencies = new Map([
 // }, 0);
 
 // console.log(balance);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+//Maximum value
+
+const max = movements.reduce(function (acc, mov) {
+  if (acc > mov) {
+    return acc;
+  } else {
+    return mov;
+  }
+}, movements[0]);
+
+console.log(max);
