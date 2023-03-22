b00fea0d8e10997fddeca5fcb84e16b5ac13aa23
oli ullah <oli.jewel@northsouth.edu>
2023-03-22 23:11:23 +0600

    Calculated income, withdrawals, and interest

--- 09- Array-Bankist/starter/script.js
+++ 09- Array-Bankist/starter/script.js
@@ -71,7 +71,7 @@ const displayMovement = function (movements) {
       i + 1
     } ${type}</div>
        <div class="movements__date">3 days ago</div>
-       <div class="movements__value">${mov}</div>
+       <div class="movements__value">${mov}€</div>
   </div>`;
     containerMovements.insertAdjacentHTML("afterbegin", html);
   });
@@ -83,6 +83,26 @@ const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
   labelBalance.textContent = balance + "£";
 };
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+
+  labelSumIn.textContent = `${incomes}€`;
+  const withdraw = movements
+    .filter((mov) => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(withdraw)}€`;
+  const interest = movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * 1.2) / 100)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+
+calcDisplaySummary(account1.movements);
+
 calcDisplayBalance(account1.movements);
 const createUsername = function (accs) {
   accs.forEach(function (acc) {
