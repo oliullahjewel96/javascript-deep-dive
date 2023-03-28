5671837faa18a55cd1c31c9ba7df0beb0bbc97be
oli ullah <oli.jewel@northsouth.edu>
2023-03-28 22:43:04 +0600

    Finished adding sorting functionality

--- 09- Array-Bankist/starter/script.js
+++ 09- Array-Bankist/starter/script.js
@@ -61,9 +61,11 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-const displayMovement = function (movements) {
+const displayMovement = function (movements, sort = false) {
   containerMovements.innerHTML = "";
-  movements.forEach(function (mov, i) {
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
     const html = ` 
     <div class="movements__row">
@@ -197,6 +199,14 @@ btnClose.addEventListener("click", function (e) {
   }
   //Clear the fields
   inputCloseUsername.value = inputClosePin.value = "";
+});
+
+let sorted = false;
+
+btnSort.addEventListener("click", function (e) {
+  e.preventDefault();
+  displayMovement(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -436,7 +446,7 @@ const currencies = new Map([
 // const account = accounts.find((acc) => acc.owner === "Jessica Davis");
 // console.log(account);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+//const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // //EQUALITY
 
@@ -485,9 +495,9 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 //SORT method
 
 //string
-const owners = ["oli", "robin", "antu", "saif"];
-console.log(owners.sort());
-console.log(owners);
+// const owners = ["oli", "robin", "antu", "saif"];
+// console.log(owners.sort());
+// console.log(owners);
 
 //Numbers
 
