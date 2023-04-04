13fc8499080db2d529bc5f5d31186d8a9cc00278
oli ullah <oli.jewel@northsouth.edu>
2023-04-04 13:09:46 +0600

    rounding decimals

--- 09- Array-Bankist/starter/script.js
+++ 09- Array-Bankist/starter/script.js
@@ -65,7 +65,7 @@ console.log(Math.max(5, 18, 3, 323, 35, 556, 6));
 console.log(Math.min(5, 18, 3, 323, 35, 556, 6));
 
 console.log(Math.PI * Number.parseFloat("10px") ** 2);
-console.log(Math.trunc(Math.random() * 6) + 1);
+console.log(Math.floor(Math.random() * 6) + 1);
 
 //Rounding integers
 console.log(Math.trunc(23.3));
@@ -78,6 +78,11 @@ console.log(Math.ceil(23.9));
 
 console.log(Math.floor(23.3));
 console.log(Math.floor(23.9));
+
+//Rounding decimals
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log((2.2345).toFixed(2));
 //
 //
 //
