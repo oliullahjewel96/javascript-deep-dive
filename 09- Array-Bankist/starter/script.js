4159c9504bc1010c81620ec1a210ac1b9dec5fd5
oli ullah <oli.jewel@northsouth.edu>
2023-04-12 12:30:20 +0600

    calculated date and added to bankist app

--- 09- Array-Bankist/starter/script.js
+++ 09- Array-Bankist/starter/script.js
@@ -17,8 +17,8 @@ const account1 = {
     "2020-04-01T10:17:24.185Z",
     "2020-05-08T14:11:59.604Z",
     "2020-05-27T17:01:17.194Z",
-    "2020-07-11T23:36:17.929Z",
-    "2020-07-12T10:51:36.790Z",
+    "2023-04-09T23:36:17.929Z",
+    "2023-04-12T10:51:36.790Z",
   ],
   currency: "EUR",
   locale: "pt-PT", // de-DE
@@ -115,6 +115,16 @@ console.log(new Date(2023, 3, 4, 14, 55, 14));
 // console.log(future.getTime());
 // console.log(Date.now()); //gives timestamp
 
+// const future = new Date(2024, 3, 4, 14, 55, 14);
+// console.log(+future);
+
+// const calcDaysPassed = (date1, date2) =>
+//   Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+// const days1 = calcDaysPassed(new Date(2023, 3, 12), new Date(2023, 3, 18));
+
+// console.log(days1);
+
 //
 //
 //
@@ -187,6 +197,24 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
+const formateMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+
+  if (daysPassed === 0) return "Today";
+  if (daysPassed === 1) return "Today";
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+  }
+};
 const displayMovement = function (acc, sort = false) {
   containerMovements.innerHTML = "";
 
@@ -197,12 +225,7 @@ const displayMovement = function (acc, sort = false) {
     const type = mov > 0 ? "deposit" : "withdrawal";
 
     const date = new Date(acc.movementsDates[i]);
-    const now = new Date();
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
-    const year = date.getFullYear();
-
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formateMovementDate(date);
 
     const html = ` 
     <div class="movements__row">
