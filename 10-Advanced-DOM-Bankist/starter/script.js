b9a8c437b0eae44f21689fbffd0377c7d7a13a7d
oli ullah <oli.jewel@northsouth.edu>
2023-05-03 18:11:28 +0600

    revealing section using observer API

--- 10-Advanced-DOM-Bankist/starter/script.js
+++ 10-Advanced-DOM-Bankist/starter/script.js
@@ -141,7 +141,7 @@ const header = document.querySelector('.header');
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
+
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
 };
@@ -152,6 +152,24 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 
 headerObserver.observe(header);
+
+//Reveal Section
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 ///////
 ///////
 //////
