b97d3c109e6a7e054d1c0743cb0e0b9d7b641220
oli ullah <oli.jewel@northsouth.edu>
2023-05-16 20:37:13 +0600

    Completed first coding challenge of OOP

--- 11-OOP/starter/script.js
+++ 11-OOP/starter/script.js
@@ -2,33 +2,62 @@
 
 //constructor function
 
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
+
+// const oli = new Person('Oli', 1996);
+// console.log(oli);
+
+// //1. A new empty object is created
+// //2. function is called, this = newly created object
+// //3. newly created object is a linked to prototype
+// //4. function automatically return {}
+
+// const fanzhendong = new Person('Fan', 1998);
+// console.log(fanzhendong);
+// console.log(oli instanceof Person);
+
+// //prototypes
+
+// console.log(Person.prototype);
+// Person.prototype.calcAge = function () {
+//   console.log(2023 - this.birthYear);
+// };
+
+// oli.calcAge();
+
+// console.log(oli.__proto__);
+// console.log(oli.__proto__ === Person.prototype);
+// console.log(Person.prototype.isPrototypeOf(oli));
+// console.log(oli.hasOwnProperty('firstName'));
+
+// console.log(oli.__proto__.__proto__);
+
+//coding challenge
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
 };
 
-const oli = new Person('Oli', 1996);
-console.log(oli);
-
-//1. A new empty object is created
-//2. function is called, this = newly created object
-//3. newly created object is a linked to prototype
-//4. function automatically return {}
-
-const fanzhendong = new Person('Fan', 1998);
-console.log(fanzhendong);
-console.log(oli instanceof Person);
-
-//prototypes
-
-console.log(Person.prototype);
-Person.prototype.calcAge = function () {
-  console.log(2023 - this.birthYear);
+Car.prototype.accelerate = function () {
+  this.speed = this.speed + 10;
+  console.log(`${this.make} going at ${this.speed} km/h`);
 };
 
-oli.calcAge();
+Car.prototype.brake = function () {
+  this.speed = this.speed - 5;
+  console.log(`${this.make} going at ${this.speed} km/h`);
+};
 
-console.log(oli.__proto__);
-console.log(oli.__proto__ === Person.prototype);
-console.log(Person.prototype.isPrototypeOf(oli));
-console.log(oli.hasOwnProperty('firstName'));
+const car1 = new Car('BMW', 110);
+const car2 = new Car('Marcedes', 100);
+
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
+car2.brake();
+car2.brake();
+car2.brake();
