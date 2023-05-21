d3664e05aa24abce54033876a3009fd0bc3c2c29
oli ullah <oli.jewel@northsouth.edu>
2023-05-21 17:32:13 +0600

    Coding challenge 2 completed

--- 11-OOP/starter/script.js
+++ 11-OOP/starter/script.js
@@ -112,3 +112,50 @@
 
 // const steven = Object.create(PersonProto);
 // console.log(steven);
+
+//coding challenge 2
+
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+
+// Car.prototype.accelerate = function () {
+//   this.speed = this.speed + 10;
+//   console.log(`${this.make} going at ${this.speed} km/h`);
+// };
+
+// Car.prototype.brake = function () {
+//   this.speed = this.speed - 5;
+//   console.log(`${this.make} going at ${this.speed} km/h`);
+// };
+
+class Car {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  accelerate() {
+    this.speed = this.speed + 10;
+    console.log(`${this.make} going at ${this.speed} km/h`);
+  }
+  brake() {
+    this.speed = this.speed - 5;
+    console.log(`${this.make} going at ${this.speed} km/h`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new Car('ford', 120);
+
+ford.accelerate();
+ford.brake();
+console.log(ford.speedUS);
+ford.speedUS = 50;
+console.log(ford);
