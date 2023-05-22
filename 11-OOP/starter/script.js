aeada20fa2b52a63209f9dd0a6480b2d0397d0ff
oli ullah <oli.jewel@northsouth.edu>
2023-05-22 12:16:09 +0600

    Completed final coding challenge of OOP

--- 11-OOP/starter/script.js
+++ 11-OOP/starter/script.js
@@ -252,3 +252,64 @@
 // acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
 
 // console.log(acc1.getMovements());
+
+// Final coding challenge
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  accelerate() {
+    this.speed = this.speed + 10;
+    console.log(`${this.make} going at ${this.speed} km/h`);
+    return this;
+  }
+  brake() {
+    this.speed = this.speed - 5;
+    console.log(`${this.make} going at ${this.speed} km/h`);
+    return this;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class EVCL extends CarCl {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(charge) {
+    this.#charge = charge;
+    return this;
+  }
+
+  accelerate() {
+    this.speed = this.speed + 10;
+    this.#charge--;
+    console.log(
+      `${this.make} going at ${this.speed} km/h, with a charge of ${
+        this.#charge
+      }%`
+    );
+    return this;
+  }
+}
+
+const rivian = new EVCL('Rivian', 120, 23);
+
+rivian
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .chargeBattery(50)
+  .accelerate();
