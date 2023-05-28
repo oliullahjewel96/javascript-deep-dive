17170c324779cd2568df5c426d1a992be9278c18
oli ullah <oli.jewel@northsouth.edu>
2023-05-28 11:58:46 +0600

    Leaflet third party map js

--- 12- Mapty/starter/script.js
+++ 12- Mapty/starter/script.js
@@ -14,7 +14,19 @@ const inputElevation = document.querySelector('.form__input--elevation');
 navigator.geolocation.getCurrentPosition(
   function (position) {
     const { latitude, longitude } = position.coords;
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude];
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
   },
   function () {
     alert('Could not get your position');
