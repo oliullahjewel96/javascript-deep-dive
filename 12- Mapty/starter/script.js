279eb713fdafd92cba5d925f17c4e796110bc999
oli ullah <oli.jewel@northsouth.edu>
2023-06-05 12:29:02 +0600

    Input validation done

--- 12- Mapty/starter/script.js
+++ 12- Mapty/starter/script.js
@@ -99,7 +99,46 @@ class App {
   }
 
   _newWorkout(e) {
+    const validInputs = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp));
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
     e.preventDefault();
+
+    //Get data from the Form
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+
+    //If workout is running, create running object
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      //check if data is valid
+      if (
+        // !Number.isFinite(distance) ||
+        // !Number.isFinite(duration) ||
+        // !Number.isFinite(cadence)
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        return alert('Input have to be positive numbers!');
+    }
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+      if (
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration)
+      )
+        return alert('Input have to be positive numbers!');
+    }
+    //If workout is cycling, create cycling object
+
+    //Add new object to workout array
+
+    //Render workout on map as marker
+
+    //Render workout on list
+
+    //Hide form + clear input
     inputDistance.value =
       inputDuration.value =
       inputCadence.value =
