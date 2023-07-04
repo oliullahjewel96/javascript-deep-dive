566f48509d662446b58a98a0c468f8c795c949d4
oli ullah <oli.jewel@northsouth.edu>
2023-07-04 23:41:10 +0600

    Asynchronous ajax call

--- 13-Asynchronous JS/starter/script.js
+++ 13-Asynchronous JS/starter/script.js
@@ -5,46 +5,104 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const getCountryData = function (country) {
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+//   console.log(request.responseText);
+
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//     let datas;
+//     for (datas in data.languages) {
+//       console.log(`${datas}: ${data.languages[datas]}`);
+//     }
+
+//     let currency;
+//     for (currency in data.currencies) {
+//       console.log(`${currency}: ${data.currencies[currency]}`);
+//     }
+//     const html = `
+//   <article class="country">
+//     <img class="country__img" src="${data.flags.png}" />
+//     <div class="country__data">
+//         <h3 class="country__name">${data.name.common}</h3>
+//         <h4 class="country__region">${data.region}</h4>
+//         <p class="country__row"><span>👫</span>${(
+//           +data.population / 1000000
+//         ).toFixed(1)} people</p>
+//         <p class="country__row"><span>🗣️</span>${data.languages[datas]}</p>
+//         <p class="country__row"><span>💰</span>${
+//           data.currencies[currency].name
+//         }</p>
+//     </div>
+// </article>
+//   `;
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+// getCountryData('uk');
+// getCountryData('usa');
+// getCountryData('switzerland');
+// getCountryData('bangladesh');
+
+const renderCountry = function (data) {
+  let datas;
+  for (datas in data.languages) {
+  }
+
+  let currency;
+  for (currency in data.currencies) {
+  }
+  const html = `
+<article class="country">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[datas]}</p>
+      <p class="country__row"><span>💰</span>${
+        data.currencies[currency].name
+      }</p>
+  </div>
+</article>
+`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
   request.send();
-  console.log(request.responseText);
 
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
-    console.log(data);
-    let datas;
-    for (datas in data.languages) {
-      console.log(`${datas}: ${data.languages[datas]}`);
-    }
+    renderCountry(data);
 
-    let currency;
-    for (currency in data.currencies) {
-      console.log(`${currency}: ${data.currencies[currency]}`);
-    }
-    const html = `
-  <article class="country">
-    <img class="country__img" src="${data.flags.png}" />
-    <div class="country__data">
-        <h3 class="country__name">${data.name.common}</h3>
-        <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span>${(
-          +data.population / 1000000
-        ).toFixed(1)} people</p>
-        <p class="country__row"><span>🗣️</span>${data.languages[datas]}</p>
-        <p class="country__row"><span>💰</span>${
-          data.currencies[currency].name
-        }</p>
-    </div>
-</article>
-  `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+    //Get neighbour country
+    const [neighbour] = data.borders;
+    console.log(neighbour);
+    if (!neighbour) return;
+
+    //AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+      renderCountry(data2);
+    });
   });
 };
 
-getCountryData('uk');
-getCountryData('usa');
-getCountryData('switzerland');
-getCountryData('bangladesh');
+getCountryAndNeighbour('argentina');
+getCountryAndNeighbour('bolivia');
