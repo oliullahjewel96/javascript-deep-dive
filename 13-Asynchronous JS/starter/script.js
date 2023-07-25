268a8d1ea1463bdc94c1094a495a9fe9c4034805
oli ullah <oli.jewel@northsouth.edu>
2023-07-26 00:40:59 +0600

    asynchronous ajax call

--- 13-Asynchronous JS/starter/script.js
+++ 13-Asynchronous JS/starter/script.js
@@ -111,7 +111,21 @@ const renderCountry = function (data, className = '') {
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      console.log(data);
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      console.log(neighbour);
+
+      if (!neighbour) return;
+
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      renderCountry(data[0], 'neighbour');
+    });
 };
 
 getCountryData('bangladesh');
