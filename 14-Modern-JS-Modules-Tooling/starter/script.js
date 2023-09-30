b8a3d02d98bace8bbce6d9ffa9e2ea4f16bd9ab1
oli ullah <oli.jewel@northsouth.edu>
2023-09-30 16:32:08 +0600

    import export as named value

---
+++ 14-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,9 @@
+//Importing modules
+// import { addToCart, totalPrice, totalQuantity } from './shoppingCart.js';
+// addToCart('bread', 5);
+
+// console.log(totalPrice, totalQuantity);
+console.log('Importing Modules');
+import * as ShoppingCart from './shoppingCart.js';
+
+ShoppingCart.addToCart('bread', 5);
---
+++ 14-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -0,0 +1,16 @@
+//Exporting Modules
+
+console.log('Exporting Modules');
+
+const shippingCost = 10;
+const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity };
