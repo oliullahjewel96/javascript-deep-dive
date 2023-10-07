d4f14b2fa83c615aa1ea0019163feaa12409ba2d
oli ullah <oli.jewel@northsouth.edu>
2023-10-07 18:43:43 +0600

    npm in js

--- 14-Modern-JS-Modules-Tooling/starter/script.js
+++ 14-Modern-JS-Modules-Tooling/starter/script.js
@@ -3,29 +3,45 @@
 // addToCart('bread', 5);
 
 // console.log(totalPrice, totalQuantity);
-console.log('Importing Modules');
+// console.log('Importing Modules');
 // import * as ShoppingCart from './shoppingCart.js';
 
 // ShoppingCart.addToCart('bread', 5);
 // console.log(ShoppingCart.totalPrice);
 
-import add from './shoppingCart.js';
+// import add from './shoppingCart.js';
 
-add('pizza', 2);
+// add('pizza', 2);
 
 // const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
 
 // const data = await res.json();
 // console.log(data);
 
-const getLastPost = async function () {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+// const getLastPost = async function () {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
 
-  const data = await res.json();
+//   const data = await res.json();
 
-  return { title: data.at(-1).title, text: data.at(-1).body };
+//   return { title: data.at(-1).title, text: data.at(-1).body };
+// };
+
+// const lastPost = await getLastPost();
+
+// console.log(lastPost);
+
+import cloneDeep from '../../node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quaantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
 };
 
-const lastPost = await getLastPost();
-
-console.log(lastPost);
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+state.user.loggedIn = false;
+console.log(stateClone);
+console.log(stateDeepClone);
